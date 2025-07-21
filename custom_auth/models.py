b2dbfa422730e33e6c4a1d0a42a48fab817de130
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-21 21:18:14 +0500

    create customuser model

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.core.validators import RegexValidator
 
-# Create your models here.
+phone_regex = RegexValidator(
+    regex=r'^\+998\d{9}$',
+    message='The phone: +998 xx xxx xx xx'
+)
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('The phone must be set')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if not extra_fields.get('is_staff'):
+            raise ValueError('The user must be is_staff=True')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('The user must be is_superuser=True')
+        return self.create_user(phone_number, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(validators=[phone_regex], max_length=13, unique=True)
+    data_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.phone_number
