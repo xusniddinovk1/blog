ebdcdc960c5762039910dd174d334d0fc72ee35b
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-22 11:07:00 +0500

    configure admin panel for models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from blog.models import AboutSite, Category, Post
 
-# Register your models here.
+admin.site.register(AboutSite)
+admin.site.register(Category)
+admin.site.register(Post)
