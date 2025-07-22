d55747764177e498e4dfc922add01350b9fe247e
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-22 11:04:20 +0500

    add category and postform

---
+++ dashboard/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from blog.models import AboutSite, Category, Post
+
+
+class AboutSiteForm(forms.ModelForm):
+    class Meta:
+        model = AboutSite
+        fields = ['title', 'description', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title'}),
+            'description': forms.Textarea(
+                attrs={'class': 'form-control', 'placeholder': 'Enter description', 'row': 5}),
+            'image': forms.FileInput(attrs={'class': 'form-control', 'onchange': 'loadFile(event)'}),
+        }
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name of category'})
+        }
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'category', 'content', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title of article'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(
+                attrs={'class': 'form-control', 'placeholder': 'Enter context of article', 'row': 5}),
+            'image': forms.TextInput(attrs={'class': 'form-control', 'onchange': 'loadFile(event)'}),
+        }
