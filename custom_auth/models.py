9a20cf978a8099181160d8fb00f7867b826afc31
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-28 17:56:31 +0500

    initial commit

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -20,9 +20,9 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        if not extra_fields.get('is_staff'):
+        if extra_fields.get('is_staff') is not True:
             raise ValueError('The user must be is_staff=True')
-        if not extra_fields.get('is_superuser'):
+        if extra_fields.get('is_superuser') is not True:
             raise ValueError('The user must be is_superuser=True')
         return self.create_user(phone_number, password, **extra_fields)
 
