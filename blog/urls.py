772118ebe5b86882fba810f7ddba50d09fa71af1
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-29 10:14:55 +0500

    initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', home_page, name='home_page'),
+    path('about/', about_page, name='about_page')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render, redirect
 from .models import AboutSite, Category, Post
 
 
-def contact(request):
-    text = AboutSite.objects.all()
-    ctx = {
-        'text': text
-    }
-    return render(request, 'blog/about_site.html', ctx)
-
-
 def home_page(request):
     categories = Category.objects.all()
     articles = Post.objects.all()
@@ -18,3 +10,11 @@ def home_page(request):
         'articles': articles
     }
     return render(request, 'blog/index.html', ctx)
+
+
+def about_page(request):
+    text = AboutSite.objects.first()
+    ctx = {
+        'text': text
+    }
+    return render(request, 'blog/about.html', ctx)
